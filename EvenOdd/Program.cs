6a23f4ff5a63b238fae1a49c665d0f61cb2a437d
Administrator <Administrator@BE66274AEDD8578>
2023-07-17 15:25:22 +0530

    Add project files.

---
+++ EvenOdd/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EvenOdd{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number:");
+            double n = double.Parse(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("Number is even");
+            }
+            else
+            {
+                Console.WriteLine("Number is odd");
+            }
+        }
+    }
+}
